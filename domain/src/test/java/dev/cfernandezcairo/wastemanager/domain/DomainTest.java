e194cb5c49be5d71d5f4aef5e1ab130948621437
Carlos Alberto Fernández Cairó <cfernandezcairo@gmail.com>
2024-04-15 07:27:53 -0400

    refactor domain tests

--- domain/src/test/java/dev/cfernandezcairo/wastemanager/domain/DomainTest.java
+++ domain/src/test/java/dev/cfernandezcairo/wastemanager/domain/DomainTest.java
@@ -8,6 +8,7 @@ import dev.cfernandezcairo.wastemanager.domain.exception.GenericSpecificationExc
 import dev.cfernandezcairo.wastemanager.domain.service.WasteCenterAuthorizationService;
 import dev.cfernandezcairo.wastemanager.domain.service.WasteManagerService;
 import dev.cfernandezcairo.wastemanager.domain.service.WasteManagerAddressService;
+import dev.cfernandezcairo.wastemanager.domain.vo.Id;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -25,8 +26,8 @@ public class DomainTest {
     @Test
     public void addCenterAuthorizationToManager(){
 
-        var managerAddress = createWasteManagerAddress(1L, true, "Waste Manager 01 Address");
-        var manager = createManager(1L, "Waste Manager 01", "nif 01", true, managerAddress);
+        var managerAddress = createWasteManagerAddress(Id.withId("1"), true, "Waste Manager 01 Address");
+        var manager = createManager(Id.withId("1"), "Waste Manager 01", "nif 01", true, managerAddress);
 
         var centerAuthorization = createCenterAuthorization("1");
         assertTrue(manager.addCenterAuthorizationToManager(centerAuthorization));
@@ -34,8 +35,8 @@ public class DomainTest {
 
     @Test
     public void addCenterAuthorizationToManager_failBecauseManagerIsNotEnable(){
-        var managerAddress = createWasteManagerAddress(2L, true, "Waste Manager 02 Address");
-        var manager = createManager(2L, "Waste Manager 02", "nif 02", false, managerAddress);
+        var managerAddress = createWasteManagerAddress(Id.withId("2"), true, "Waste Manager 02 Address");
+        var manager = createManager(Id.withId("2"), "Waste Manager 02", "nif 02", false, managerAddress);
 
         var centerAuthorization = createCenterAuthorization("2");
         assertThrows(GenericSpecificationException.class, () -> manager.addCenterAuthorizationToManager(centerAuthorization));
@@ -43,8 +44,8 @@ public class DomainTest {
 
     @Test
     public void removeCenterAuthorization(){
-        var managerAddress = createWasteManagerAddress(3L, true, "Waste Manager 03 Address");
-        var manager = createManager(3L, "Waste Manager 03", "nif 03", true, managerAddress);
+        var managerAddress = createWasteManagerAddress(Id.withId("3"), true, "Waste Manager 03 Address");
+        var manager = createManager(Id.withId("3"), "Waste Manager 03", "nif 03", true, managerAddress);
 
         var centerAuthorization = createCenterAuthorization("3");
         manager.addCenterAuthorizationToManager(centerAuthorization);
@@ -58,10 +59,10 @@ public class DomainTest {
     public void filterWasteManagerByStatus(){
         List<WasteManager> wasteManagers = new ArrayList<>();
 
-        var managerAddress = createWasteManagerAddress(5L, true,"Waste Manager Address");
+        var managerAddress = createWasteManagerAddress(Id.withId("5"), true,"Waste Manager Address");
 
-        var managerEnabled = createManager(5L, "Waste Manager 05", "nif 05", true, managerAddress);
-        var managerDisabled = createManager(6L, "Waste Manager 06", "nif 06", false, managerAddress);
+        var managerEnabled = createManager(Id.withId("5"), "Waste Manager 05", "nif 05", true, managerAddress);
+        var managerDisabled = createManager(Id.withId("6"), "Waste Manager 06", "nif 06", false, managerAddress);
 
         wasteManagers.add(managerEnabled);
         wasteManagers.add(managerDisabled);
@@ -81,10 +82,10 @@ public class DomainTest {
     public void filterWasteManagerByNIF(){
         List<WasteManager> wasteManagers = new ArrayList<>();
 
-        var managerAddress = createWasteManagerAddress(6L, true, "Waste Manager Address");
+        var managerAddress = createWasteManagerAddress(Id.withId("6"), true, "Waste Manager Address");
 
-        var managerNIF0 = createManager(7L, "Waste Manager 07", "nif 07", true, managerAddress);
-        var managerNIF1 = createManager(8L, "Waste Manager 08", "nif 18", false, managerAddress);
+        var managerNIF0 = createManager(Id.withId("7"), "Waste Manager 07", "nif 07", true, managerAddress);
+        var managerNIF1 = createManager(Id.withId("8"), "Waste Manager 08", "nif 18", false, managerAddress);
 
         wasteManagers.add(managerNIF0);
         wasteManagers.add(managerNIF1);
@@ -104,7 +105,7 @@ public class DomainTest {
     public void filterWastManagerAddressByDireccion(){
         List<WasteManagerAddress> address = new ArrayList<>();
 
-        var managerAddress = createWasteManagerAddress(7L, true,"Waste Manager Address");
+        var managerAddress = createWasteManagerAddress(Id.withId("7"), true,"Waste Manager Address");
         address.add(managerAddress);
 
         var actualAddress = WasteManagerAddressService.filterAndRetrieveWasteManagerAddress(address,
@@ -127,11 +128,11 @@ public class DomainTest {
 
     @Test
     public void findWasteManagerById(){
-        Map<Long, WasteManager> wasteManagers = new HashMap<>();
+        Map<Id, WasteManager> wasteManagers = new HashMap<>();
 
-        var managerAddress = createWasteManagerAddress(8L, true, "Waste Manager Address");
-        var manager_1 = createManager(9L, "Waste Manager 09", "nif 09", true, managerAddress);
-        var manager_2 = createManager(10L, "Waste Manager 10", "nif 10", false, managerAddress);
+        var managerAddress = createWasteManagerAddress(Id.withId("8"), true, "Waste Manager Address");
+        var manager_1 = createManager(Id.withId("9"), "Waste Manager 09", "nif 09", true, managerAddress);
+        var manager_2 = createManager(Id.withId("10"), "Waste Manager 10", "nif 10", false, managerAddress);
 
         wasteManagers.put(manager_1.getId(), manager_1);
         wasteManagers.put(manager_2.getId(), manager_2);
@@ -143,9 +144,9 @@ public class DomainTest {
 
     @Test
     public void findWasteManagerAddressById(){
-        Map<Long, WasteManagerAddress> wasteManagerAddress = new HashMap<>();
+        Map<Id, WasteManagerAddress> wasteManagerAddress = new HashMap<>();
 
-        var managerAddress_1 = createWasteManagerAddress(5L, true,"Waste MAnager Address");
+        var managerAddress_1 = createWasteManagerAddress(Id.withId("5"), true,"Waste MAnager Address");
 
         wasteManagerAddress.put(managerAddress_1.getId(), managerAddress_1);
 
@@ -154,9 +155,9 @@ public class DomainTest {
         assertEquals(expectedId, actualId);
     }
 
-    private WasteManagerAddress createWasteManagerAddress(Long id, Boolean isEnabled, String direccion){
+    private WasteManagerAddress createWasteManagerAddress(Id id, Boolean isEnabled, String direccion){
         return WasteManagerAddress.builder().
-                id(1L).
+                id(id).
                 isEnabled(isEnabled).
                 direccion(direccion).
                 build();
@@ -165,7 +166,7 @@ public class DomainTest {
 
     private WasteCenterAuthorization createCenterAuthorization(String authorizationNumber){
         return WasteCenterAuthorization.builder().
-                id(Long.valueOf(authorizationNumber)).
+                id(Id.withId(authorizationNumber)).
                 authorizationNumber(authorizationNumber).
                 build();
     }
@@ -176,9 +177,9 @@ public class DomainTest {
         return listOfCenterAuthorization;
     }
 
-    private WasteManager createManager(Long id, String nombre, String nif, Boolean isEnabled, WasteManagerAddress wasteManagerAddress){
+    private WasteManager createManager(Id id, String nombre, String nif, Boolean isEnabled, WasteManagerAddress wasteManagerAddress){
         return WasteManager.builder().
-                id(1L).
+                id(id).
                 nombre(nombre).
                 nif(nif).
                 wasteManagerAddress(wasteManagerAddress).
